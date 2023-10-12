50607d447e67aaf9c1da990790e76fec3c6f5b0b
referanslar <147508935+referanslar@users.noreply.github.com>
2023-10-12 22:29:22 +0300

    feat(leetcode): add solution for problem `2703`

---
+++ leetcode/2703.js
@@ -0,0 +1,13 @@
+/**
+ * Title: 2703. Return Length of Arguments Passed
+ * URL: https://leetcode.com/problems/return-length-of-arguments-passed/
+ * Answered by @referanslar
+ */
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
