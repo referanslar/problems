6409999fbb11b015d2f08add982bac101895d10b
referanslar <147508935+referanslar@users.noreply.github.com>
2023-10-24 18:42:13 +0300

    feat(leetcode): add solution for problem `2235`

---
+++ leetcode/2235.js
@@ -0,0 +1,14 @@
+/**
+ * Title: 2235. Add Two Integers
+ * URL: https://leetcode.com/problems/add-two-integers/
+ * Answered by @referanslar
+ */
+
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function (num1, num2) {
+  return num1 + num2;
+};
