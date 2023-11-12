4b176f17c4d934b5e27c9d74f8d2aeeb64a9323d
referanslar <147508935+referanslar@users.noreply.github.com>
2023-11-12 16:44:30 +0300

    feat(leetcode): add solution for problem `1757`

---
+++ leetcode/1757/1757.sql
@@ -0,0 +1,7 @@
+/**
+ * Title: 1757. Recyclable and Low Fat Products
+ * URL: https://leetcode.com/problems/recyclable-and-low-fat-products/
+ * Answered by @referanslar
+ */
+
+SELECT product_id FROM Products WHERE low_fats = 'Y' and recyclable = 'Y';
